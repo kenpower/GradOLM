b7e1be8b1dbb1232dd4b30608049ee0158612f1e
Ken Power <github@kenpower.com>
2024-09-06 11:44:07 +0100

    first build

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  build: {
+    outDir: "..", // Change this to your preferred output folder
+    emptyOutDir: false, // Optionally clean the folder before building
+  },
+});
